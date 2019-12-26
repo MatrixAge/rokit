8907f674a3287ada49ce31630e61ee1b15b1e399
wendao <xiewendao123@foxmail.com>
2019-12-26 11:58:59 +0800

    [add] suitable for nextjs

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -34,7 +34,7 @@ const Index: React.FC<IProps> = (props) => {
 
             const { current } = img_ref
 
-            if (width && height && container_ref.current) {
+            if (document && width && height && container_ref.current) {
                   if (width === '100%') {
                         _width = container_ref.current.offsetWidth
                   } else {
